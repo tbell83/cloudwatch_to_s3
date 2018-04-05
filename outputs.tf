aef7b471b52da3601662ebf4aee4fcde82b617c7
Thomas Bell <tbell@dramafever.com>
2018-04-05 09:11:34 -0400

    i wish they would add count support in modules

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "firehose_to_s3_arn" {
   description = "ARN of the firehose to s3 role"
-  value       = "${aws_iam_role.firehose_to_s3.arn}"
+  value       = "${var.count == "0" ? "null" : join(",", aws_iam_role.firehose_to_s3.*.arn)}"
 }
 
 output "cloudwatch_to_firehose_arn" {
   description = "ARN of the cloudwatch to firehose role"
-  value       = "${aws_iam_role.cloudwatch_to_firehose.arn}"
+  value       = "${var.count == "0" ? "null" : join(",", aws_iam_role.cloudwatch_to_firehose.*.arn)}"
 }
