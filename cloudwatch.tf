f25d3e8521828b97163f8a5d46b3d1e36c0f9171
Thomas Bell <tbell@dramafever.com>
2018-04-17 10:06:30 -0400

    use prefix in name

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_log_subscription_filter" "subscription_filter" {
   count = "${var.count == "0" ? 0 : length(var.logs)}"
 
-  name            = "${var.name}_cloudwatch_logfilter_${count.index}"
+  name            = "${var.name}_cloudwatch_logfilter_${element(var.logs[count.index], 0)}"
   role_arn        = "${aws_iam_role.cloudwatch_to_firehose.arn}"
   log_group_name  = "${element(var.logs[count.index], 0)}"
   filter_pattern  = "${element(var.logs[count.index], 1)}"
