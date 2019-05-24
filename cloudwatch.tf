d60d54fe8d1e46e3fe5e8f6bbda6bcc8595530ba
Thomas Bell <tbell@dramafever.com>
2019-05-24 08:57:47 -0400

    tf12

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,10 +1,11 @@
 resource "aws_cloudwatch_log_subscription_filter" "subscription_filter" {
-  count = "${var.mod_count == "0" ? 0 : length(var.logs)}"
+  count = var.mod_count == "0" ? 0 : length(var.logs)
 
   name            = "${var.name}_cloudwatch_logfilter_${element(var.logs[count.index], 0)}"
-  role_arn        = "${aws_iam_role.cloudwatch_to_firehose.arn}"
-  log_group_name  = "${element(var.logs[count.index], 0)}"
-  filter_pattern  = "${element(var.logs[count.index], 1)}"
-  destination_arn = "${aws_kinesis_firehose_delivery_stream.cloudwatch_to_s3_stream.*.arn[count.index]}"
-  distribution    = "${var.subscription_filter_distribution}"
+  role_arn        = aws_iam_role.cloudwatch_to_firehose[0].arn
+  log_group_name  = element(var.logs[count.index], 0)
+  filter_pattern  = element(var.logs[count.index], 1)
+  destination_arn = aws_kinesis_firehose_delivery_stream.cloudwatch_to_s3_stream[count.index].arn
+  distribution    = var.subscription_filter_distribution
 }
+
