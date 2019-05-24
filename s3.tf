acc6319529006e7a631a7861d41475491343d5c9
Tom Bell <tbell@tombellnj.com>
2019-05-24 10:21:59 -0400

    Revert "tf12"

--- s3.tf
+++ s3.tf
@@ -1,45 +1,35 @@
 resource "aws_s3_bucket" "cloudwatch_logging_bucket" {
-  count  = var.mod_count == "0" ? 0 : var.target_bucket_arn != "0" ? 0 : 1
+  count  = "${var.mod_count == "0" ? 0 : var.target_bucket_arn != "0" ? 0 : 1}"
   bucket = "${var.name}_cloudwatch_logging_bucket"
-  tags   = var.tags
+  tags   = "${var.tags}"
 }
 
 resource "aws_s3_bucket_policy" "cloudwatch_logging_bucket" {
-  count  = var.mod_count == "0" ? 0 : var.target_bucket_arn != "0" ? 0 : 1
-  bucket = aws_s3_bucket.cloudwatch_logging_bucket[0].id
-  policy = data.aws_iam_policy_document.cloudwatch_logging_bucket[0].json
+  count  = "${var.mod_count == "0" ? 0 : var.target_bucket_arn != "0" ? 0 : 1}"
+  bucket = "${aws_s3_bucket.cloudwatch_logging_bucket.id}"
+  policy = "${data.aws_iam_policy_document.cloudwatch_logging_bucket.json}"
 }
 
 data "aws_iam_policy_document" "cloudwatch_logging_bucket" {
-  count = var.mod_count == "0" ? 0 : var.target_bucket_arn != "0" ? 0 : 1
+  count = "${var.mod_count == "0" ? 0 : var.target_bucket_arn != "0" ? 0 : 1}"
 
   statement {
     sid     = "DefaultDeny"
     actions = ["s3:*"]
 
     resources = [
-      aws_s3_bucket.cloudwatch_logging_bucket[0].id,
-      "${aws_s3_bucket.cloudwatch_logging_bucket[0].id}/*",
+      "${aws_s3_bucket.cloudwatch_logging_bucket.id}",
+      "${aws_s3_bucket.cloudwatch_logging_bucket.id}/*",
     ]
 
     condition {
       test     = "StringNotLike"
       variable = "aws:userId"
 
-      # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-      # force an interpolation expression to be interpreted as a list by wrapping it
-      # in an extra set of list brackets. That form was supported for compatibilty in
-      # v0.11, but is no longer supported in Terraform v0.12.
-      #
-      # If the expression in the following list itself returns a list, remove the
-      # brackets to avoid interpretation as a list of lists. If the expression
-      # returns a single list item then leave it as-is and remove this TODO comment.
-      values = [
-        compact(
-          [var.s3_access_uids, aws_iam_role.firehose_to_s3[0].unique_id],
-        ),
-      ]
+      values = ["${compact(list(
+        "${var.s3_access_uids}",
+        "${aws_iam_role.firehose_to_s3.unique_id}"
+      ))}"]
     }
   }
 }
-
